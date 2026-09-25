ae78b00ee3c95168b754dba26c7524a7d892c6be
sujittomarnotes <asujittomar@gmail.com>
2026-09-25 14:18:01 +0000

    updated

--- 01fundamental.js
+++ 01fundamental.js
@@ -3,6 +3,28 @@
 // What is Javascript?
 
 /*
-Java script is lightweight interpreted programming language with first-class function. While it is most well -known as the scripting language for web pages.
+Java script is lightweight, cross plateform, interpreted programming language with first-class function. While it is most well -known as the scripting language for web pages.
 */
+
+// Container Type
+
+var myName = "Sujit Kumar Singh"
+let myname = "Sujit Kumar Singh"
+const MyName = "Sujit Kumar Singh"
+
+console.log(myName)
+console.log(myname)
+console.log(MyName)
+
+myName = "abc"
+myname = "def"
+// MyName = "ghi"  - Not Reassignable
+
+console.log(myName)
+console.log(myname)
+console.log(MyName)
+
+
+
+
 
--- 02variablesAndDataType.js
+++ 02variablesAndDataType.js
@@ -1,3 +1,4 @@
+// Premitive Data Type
 let coderName = "Sujit Kumar Singh";
 let coderAge = 26;
 let coderIsLogin = false;
@@ -10,18 +11,31 @@ let coderBalanceBOB = BigInt(9999999999999999n);
 let coderUserID = Symbol(121);
 let userAdminID = Symbol(121);
 
-console.log(`Type of Data for coderName ${coderName} is ${typeof coderName}`);
-console.log(`Type of Data for coderAge ${coderAge} is ${typeof coderAge}`)
-console.log(`Type of Data for coderIsLogin ${coderIsLogin} is ${typeof coderIsLogin}`)
-console.log(`Type of Data for coderMarks ${coderMarks} is ${typeof coderMarks}`)
-console.log(`Type of Data for coderMobNum ${coderMobNum} is ${typeof coderMobNum}`)
-console.log(`Type of Data for coderEmail ${coderEmail} is ${typeof coderEmail}`)
-console.log(`Type of Data for coderBalanceSBI ${coderBalanceSBI} is ${typeof coderBalanceSBI}`)
-console.log(`Type of Data for coderBalanceBOI ${coderBalanceBOI} is ${typeof coderBalanceBOI}`)
-console.log(`Type of Data for coderBalanceBOB ${coderBalanceBOB} is ${typeof coderBalanceBOB}`)
-console.log(`Type of Data for coderUserID ${String(coderUserID)} is ${typeof coderUserID}`);
-console.log(coderUserID == userAdminID)
-console.log(Number.MAX_SAFE_INTEGER);
+// Non - Premitive Data Type
+let marks = {
+    math : 83,
+    eng : 63,
+    CSc : 98,
+    hin : 94,
+    sci: 88,
+}
+
+let book = ["Math", "Eng", "CSE", "HIN", "SCI"]
+
+console.log(`Type of Data for coderName ${coderName} is ${typeof coderName}`); // string
+console.log(`Type of Data for coderAge ${coderAge} is ${typeof coderAge}`) // number
+console.log(`Type of Data for coderIsLogin ${coderIsLogin} is ${typeof coderIsLogin}`) // boolean
+console.log(`Type of Data for coderMarks ${coderMarks} is ${typeof coderMarks}`) // number
+console.log(`Type of Data for coderMobNum ${coderMobNum} is ${typeof coderMobNum}`) // object
+console.log(`Type of Data for coderEmail ${coderEmail} is ${typeof coderEmail}`) // undefined
+console.log(`Type of Data for coderBalanceSBI ${coderBalanceSBI} is ${typeof coderBalanceSBI}`) // bigint
+console.log(`Type of Data for coderBalanceBOI ${coderBalanceBOI} is ${typeof coderBalanceBOI}`) // bigint
+console.log(`Type of Data for coderBalanceBOB ${coderBalanceBOB} is ${typeof coderBalanceBOB}`) // bigint
+console.log(`Type of Data for coderUserID ${String(coderUserID)} is ${typeof coderUserID}`); // Symbol
+console.log(`Type of Data for marks ${marks} is ${typeof marks}`); // Object
+console.log(`Type of Data for book ${book} is ${typeof book}`); // Object
+console.log(coderUserID == userAdminID) // false
+console.log(Number.MAX_SAFE_INTEGER); // 9007199254740991
 
 
 
