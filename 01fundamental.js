4d33e6674f0c62d60083d75d19f25a8216d9b8b6
sujittomarnotes <asujittomar@gmail.com>
2026-09-25 13:57:57 +0000

    updated

---
+++ 01fundamental.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// What is Javascript?
+
+/*
+Java script is lightweight interpreted programming language with first-class function. While it is most well -known as the scripting language for web pages.
+*/
+
---
+++ 02variablesAndDataType.js
@@ -0,0 +1,27 @@
+let coderName = "Sujit Kumar Singh";
+let coderAge = 26;
+let coderIsLogin = false;
+let coderMarks = 99.99;
+let coderMobNum = null;
+let coderEmail = undefined;
+let coderBalanceSBI = BigInt(9999999999999999);
+let coderBalanceBOI = 9999999999999999n;
+let coderBalanceBOB = BigInt(9999999999999999n);
+let coderUserID = Symbol(121);
+let userAdminID = Symbol(121);
+
+console.log(`Type of Data for coderName ${coderName} is ${typeof coderName}`);
+console.log(`Type of Data for coderAge ${coderAge} is ${typeof coderAge}`)
+console.log(`Type of Data for coderIsLogin ${coderIsLogin} is ${typeof coderIsLogin}`)
+console.log(`Type of Data for coderMarks ${coderMarks} is ${typeof coderMarks}`)
+console.log(`Type of Data for coderMobNum ${coderMobNum} is ${typeof coderMobNum}`)
+console.log(`Type of Data for coderEmail ${coderEmail} is ${typeof coderEmail}`)
+console.log(`Type of Data for coderBalanceSBI ${coderBalanceSBI} is ${typeof coderBalanceSBI}`)
+console.log(`Type of Data for coderBalanceBOI ${coderBalanceBOI} is ${typeof coderBalanceBOI}`)
+console.log(`Type of Data for coderBalanceBOB ${coderBalanceBOB} is ${typeof coderBalanceBOB}`)
+console.log(`Type of Data for coderUserID ${String(coderUserID)} is ${typeof coderUserID}`);
+console.log(coderUserID == userAdminID)
+console.log(Number.MAX_SAFE_INTEGER);
+
+
+
